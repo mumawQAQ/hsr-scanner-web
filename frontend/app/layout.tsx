e3c450eb305b01cc2121c5f119dc6552cc2d3846
muma <wgr5600133@gmail.com>
2024-12-03 18:37:46 -0500

    chore(frontend): init frontend project

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,36 @@
+import type {Metadata} from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import React from "react";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "HSR-Scanner",
+  description: "Scanner that help analysis HSR relics",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

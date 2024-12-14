ee5becfff01975f810906f668bc05c469b29d1b1
mumawQAQ <GUW18@pitt.edu>
2024-12-14 00:58:11 -0800

    chore(middleware): add api public route

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,11 @@
 import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/public(.*)', '/'])
+const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/public(.*)',
+    '/',
+    '/api/public(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {

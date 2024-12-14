e9a56f743a1a2ce3cd18993a8462d2e02701c099
mumawQAQ <GUW18@pitt.edu>
2024-12-13 21:14:12 -0800

    chore(frontend): make default page public

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,6 @@
 import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/public(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/public(.*)', '/'])
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {

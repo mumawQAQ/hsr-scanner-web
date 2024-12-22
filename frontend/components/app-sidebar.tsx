c6f4e93e2468483a72826192e52a1fdfa4d8d6ce
mumawQAQ <GUW18@pitt.edu>
2024-12-21 20:07:05 -0800

    chore: dynamically render sidebar base on the constant

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -4,27 +4,100 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
-    SidebarGroupContent,
     SidebarGroupLabel,
     SidebarHeader,
-    SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
     SidebarMenuSub,
     SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
 import {Collapsible, CollapsibleContent, CollapsibleTrigger} from "@/components/ui/collapsible";
-import {ChevronDown} from "lucide-react";
 import React from "react";
 import {useRouter} from "next/navigation";
 import {ModeToggle} from "@/components/node-toggle";
 import {UserButton} from "@clerk/nextjs";
+import {ChevronDown} from "lucide-react";
+
 
 const HSR_SCANNER_PATH = '/public/hsr-scanner'
+
 const HSR_SCANNER_MAP = {
     install: `${HSR_SCANNER_PATH}/install`,
     update_log: `${HSR_SCANNER_PATH}/update-log`,
 }
+
+type sidebarRouteType = { name: string, subTriggers: { name: string, routes: { name: string, path: string }[] }[] }
+
+const SIDEBAR_ROUTES: sidebarRouteType[] = [
+    {
+        name: 'HSR Scanner',
+        subTriggers: [
+            {
+                name: '快速开始',
+                routes: [
+                    {
+                        name: '安装',
+                        path: HSR_SCANNER_MAP.install
+                    },
+                    {
+                        name: '使用教程',
+                        path: ''
+                    }
+                ]
+            },
+            {
+                name: '开发指南',
+                routes: [
+                    {
+                        name: '项目架构介绍',
+                        path: ''
+                    },
+                    {
+                        name: '环境配置',
+                        path: ''
+                    },
+                    {
+                        name: '常见配置',
+                        path: ''
+                    }
+                ]
+
+            },
+            {
+                name: '关于',
+                routes: [
+                    {
+                        name: '更新日志',
+                        path: HSR_SCANNER_MAP.update_log
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'HSR Scanner 模板配置工具',
+        subTriggers: [
+            {
+                name: '工具',
+                routes: [
+                    {
+                        name: '模板列表',
+                        'path': ''
+                    }
+                ]
+            },
+            {
+                name: '关于',
+                routes: [
+                    {
+                        name: '更新日志',
+                        path: ''
+                    }
+                ]
+            }
+        ]
+    }
+]
 
 export function AppSidebar() {
     const router = useRouter()
@@ -34,168 +107,49 @@ export function AppSidebar() {
         router.push(path)
     }
 
-
     return (
         <Sidebar variant="inset">
             <SidebarHeader/>
             <SidebarContent>
-                <Collapsible defaultOpen>
-                    <SidebarGroup>
-                        <SidebarGroupLabel asChild>
-                            <CollapsibleTrigger>
-                                HSR Scanner
-                                <ChevronDown
-                                    className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180"/>
-                            </CollapsibleTrigger>
-                        </SidebarGroupLabel>
-                        <CollapsibleContent>
-                            <SidebarGroupContent>
-                                <SidebarMenu>
-                                    <Collapsible defaultOpen>
-                                        <SidebarMenuItem>
-                                            <CollapsibleTrigger asChild>
-                                                <SidebarMenuButton>
-                                                    快速开始
-                                                </SidebarMenuButton>
-                                            </CollapsibleTrigger>
-                                            <CollapsibleContent>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton onClick={() => {
-                                                            handleRoute(HSR_SCANNER_MAP.install)
-                                                        }}>
-                                                            安装
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton>
-                                                            使用教程
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton>
-                                                            常见问题
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                            </CollapsibleContent>
-                                        </SidebarMenuItem>
-                                    </Collapsible>
-                                    <Collapsible defaultOpen>
-                                        <SidebarMenuItem>
-                                            <CollapsibleTrigger asChild>
-                                                <SidebarMenuButton>
-                                                    开发指南
-                                                </SidebarMenuButton>
-                                            </CollapsibleTrigger>
-                                            <CollapsibleContent>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton>
-                                                            项目架构介绍
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton>
-                                                            环境配置
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton>
-                                                            常见问题
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                            </CollapsibleContent>
-                                        </SidebarMenuItem>
-                                    </Collapsible>
-                                    <Collapsible defaultOpen>
-                                        <SidebarMenuItem>
-                                            <CollapsibleTrigger asChild>
-                                                <SidebarMenuButton>
-                                                    关于
-                                                </SidebarMenuButton>
-                                            </CollapsibleTrigger>
-                                            <CollapsibleContent>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton onClick={() => {
-                                                            handleRoute(HSR_SCANNER_MAP.update_log)
-                                                        }}>
-                                                            更新日志
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                            </CollapsibleContent>
-                                        </SidebarMenuItem>
-                                    </Collapsible>
-                                </SidebarMenu>
-                            </SidebarGroupContent>
-                        </CollapsibleContent>
+                {SIDEBAR_ROUTES.map((route, index) => (
+                    <Collapsible key={index} defaultOpen>
+                        <SidebarGroup>
 
-                    </SidebarGroup>
-                </Collapsible>
-                <Collapsible defaultOpen>
-                    <SidebarGroup>
-                        <SidebarGroupLabel asChild>
-                            <CollapsibleTrigger>
-                                HSR Scanner 模板配置工具
-                                <ChevronDown
-                                    className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180"/>
-                            </CollapsibleTrigger>
-                        </SidebarGroupLabel>
-                        <CollapsibleContent>
-                            <SidebarGroupContent>
-                                <SidebarMenu>
-                                    <Collapsible defaultOpen>
-                                        <SidebarMenuItem>
-                                            <CollapsibleTrigger asChild>
-                                                <SidebarMenuButton>
-                                                    工具
-                                                </SidebarMenuButton>
-                                            </CollapsibleTrigger>
-                                            <CollapsibleContent>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton>
-                                                            模板列表
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                            </CollapsibleContent>
-                                        </SidebarMenuItem>
-                                    </Collapsible>
-                                    <Collapsible defaultOpen>
-                                        <SidebarMenuItem>
-                                            <CollapsibleTrigger asChild>
-                                                <SidebarMenuButton>
-                                                    关于
-                                                </SidebarMenuButton>
-                                            </CollapsibleTrigger>
-                                            <CollapsibleContent>
-                                                <SidebarMenuSub>
-                                                    <SidebarMenuSubItem>
-                                                        <SidebarMenuButton>
-                                                            更新日志
-                                                        </SidebarMenuButton>
-                                                    </SidebarMenuSubItem>
-                                                </SidebarMenuSub>
-                                            </CollapsibleContent>
-                                        </SidebarMenuItem>
-                                    </Collapsible>
-                                </SidebarMenu>
-                            </SidebarGroupContent>
-                        </CollapsibleContent>
-                    </SidebarGroup>
-                </Collapsible>
+                            <SidebarGroupLabel asChild>
+                                <CollapsibleTrigger>
+                                    {route.name}
+                                    <ChevronDown
+                                        className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180"/>
+                                </CollapsibleTrigger>
+                            </SidebarGroupLabel>
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    {route.subTriggers.map((sub, subIndex) => (
+                                        <Collapsible key={subIndex} defaultOpen>
+                                            <SidebarMenuItem>
+                                                <CollapsibleTrigger asChild>
+                                                    <SidebarMenuButton>{sub.name}</SidebarMenuButton>
+                                                </CollapsibleTrigger>
+                                                <CollapsibleContent>
+                                                    <SidebarMenuSub>
+                                                        {sub.routes.map((item, itemIndex) => (
+                                                            <SidebarMenuSubItem key={itemIndex}>
+                                                                <SidebarMenuButton
+                                                                    onClick={() => handleRoute(item.path)}>
+                                                                    {item.name}
+                                                                </SidebarMenuButton>
+                                                            </SidebarMenuSubItem>
+                                                        ))}
+                                                    </SidebarMenuSub>
+                                                </CollapsibleContent>
+                                            </SidebarMenuItem>
+                                        </Collapsible>
+                                    ))}
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarGroup>
+                    </Collapsible>
+                ))}
             </SidebarContent>
             <SidebarFooter className="flex items-center">
                 <ModeToggle/>
